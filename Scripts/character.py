86a18b6f900ea5db68dc585636b7a803d5192889
Jack Tsui <jacktsui23@gmail.com>
2025-01-24 12:37:13 -0600

    Implementing damage and healing system for the player.

--- Scripts/character.py
+++ Scripts/character.py
@@ -22,8 +22,35 @@ class Player(Character):
         super().__init__(game, "player", pos, size)
 
         self.attacking = False
-        
+        self.maxSp = 100
+
+        # Skill points will be the unit expensed when using a skill.
+        self.currentSp = self.maxSp
+
+        # Need this incase player is fighting from the left or right side.
+        self.attackFlip = False
+        self.attackDmg = 10
+        self.infectRate = 0.5
+
+        self.maxHp = 80
+        self.currentHp = self.maxHp
+
+    def takeDmg(self, amount):
+
+        # Reduces the current hp of player by amount of inflicted attack.
+        # Have to use max(0, ...) or the player's health 
+        # will eventually become negative.
+        self.currentHp = max(0, self.currentHp - amount)
+
+        if self.currentHp <= 0:
+            print("Player has been defeated.")
        
+    def heal(self, amount):
+
+        # Increases the current hp of player by amount of healing.
+        # Have to use min(eslf.maxHp, ...) or the player's health 
+        # will eventually overcap.
+        self.currentHp = min(self.maxHp, self.currentHp + amount)
 
     
 
