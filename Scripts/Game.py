247ecfddc9efa2a94c6efc42dbca1293a5564a3a
Jack Tsui <jacktsui23@gmail.com>
2025-01-23 18:48:21 -0600

    Setting up the main game script.

---
+++ Scripts/Game.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+
+class Game:
+    
+    def __init__(self):
+
+         # Starts up Pygame
+        pygame.init()
+
+        # Sets the name of the window icon to "Rogue-like"
+        pygame.display.set_caption("Rogue")
