0acfe84dbd864852d148cbecac319fc356f40d2d
Jack Tsui <jacktsui23@gmail.com>
2025-01-23 18:53:04 -0600

    Making game script.

--- Scripts/Game.py
+++ Scripts/Game.py
@@ -10,3 +10,10 @@ class Game:
 
         # Sets the name of the window icon to "Rogue-like"
         pygame.display.set_caption("Rogue")
+
+        # Creating a screen variable with the window dimension variables set above
+        # when setting window dimensions have to do .set_mode( (_,_) )
+        # Treat the (_,_) as order pairs inside of ( (_,_) )
+        self.screen = pygame.display.set_mode((1280 , 720 ))
+
+        self.display = pygame.Surface((640, 360))
