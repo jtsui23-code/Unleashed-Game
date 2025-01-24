347905ff34c66ae43b836bfda3d1da2ce8374367
Jack Tsui <jacktsui23@gmail.com>
2025-01-24 14:31:42 -0600

    Added image cropper for resizing resolution of sprites.

--- Scripts/enemies.py
+++ Scripts/enemies.py
@@ -10,5 +10,24 @@ class Enemy(Character):
         self.maxHp = 100
         self.currentHp = self.maxHp
         self.skillCooldown = []
-        
+
         self.attackFlip = False
+
+    def takeDmg(self, amount):
+
+        # Reduces the current hp of enemy by amount of inflicted attack.
+        # Have to use max(0, ...) or the enemy's health 
+        # will eventually become negative.
+        self.currentHp = max(0, self.currentHp - amount)
+
+        if self.currentHp <= 0:
+            print("Enemy has been defeated.")
+       
+    def heal(self, amount):
+
+        # Increases the current hp of enemy by amount of healing.
+        # Have to use min(eslf.maxHp, ...) or the enemy's health 
+        # will eventually overcap.
+        self.currentHp = min(self.maxHp, self.currentHp + amount)
+
+    
