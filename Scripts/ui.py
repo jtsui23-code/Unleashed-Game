dcaefa5eebf2a2995b132decc830f2dadb0c503f
Jack Tsui <jacktsui23@gmail.com>
2025-01-25 15:23:03 -0600

    Removed second secreen and working on the main menu.

--- Scripts/ui.py
+++ Scripts/ui.py
@@ -12,6 +12,7 @@ BLACK = (0, 0, 0)
 GRAY = (200, 200, 200)
 LIGHT_GRAY = (220, 220, 220)
 
+
 class TextBox:
     def __init__(self, x, y, width, height, text=''):
         self.rect = pygame.Rect(x, y, width, height)
