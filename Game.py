7269480b11a6c5e9238448de10f7ad9acd223343
Jack Tsui <jacktsui23@gmail.com>
2025-03-23 17:07:43 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -20,7 +20,7 @@ class Game:
         # Sets the name of the window icon to "Rogue-like"
         pygame.display.set_caption("Unleeched")
 
-        self.screenWidth = 128000
+        self.screenWidth = 1280
         self.screenHeight = 720
 
         # Creating a screen variable with the window dimension variables set above
